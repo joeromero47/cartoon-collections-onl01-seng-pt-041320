d9f1131697a05d3f3fe8c0c79d58ba8b6fccc0ad
Joseph Romero <joeromero47@gmail.com>
2020-04-22 22:14:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array) # code an argument here
+  i = 0
+  dwarves_names = []
+    while i < array
+      dwarves_names << array.
+      
 end
 
 def summon_captain_planet# code an argument here
